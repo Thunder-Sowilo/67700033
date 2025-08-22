431c9ed6825ddfbef30d80adaa820778f4565047
Vinithorn S. <Thunder.Sowilo>
2025-08-22 11:06:08 +0700

    ahh

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h2>Vinithorn Sawangwong ID:67700033</h2>
-    <h3>แคปชั่นไม่รู้ แคปหมูแน่นอน</h3>
+    <h3>Information Tecnology</h3>
   </div>
 </template>
